5305ed243621b3f9a29512193a14164660fe6b41
Jessica Goulding <jgoulding2@gmail.com>
2014-08-08 15:22:19 -0600

    added requires to test helper for lib files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative "../lib/command_line_interface"
+require_relative "../lib/cleaner.rb"
+require_relative "../lib/attendee_repo.rb"
