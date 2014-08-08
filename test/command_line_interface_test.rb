bea33d729b4e17d3b9c710a62522df990bb83581
Jessica Goulding <jgoulding2@gmail.com>
2014-08-08 13:27:38 -0600

    first commit with some skeleton files

---
+++ test/command_line_interface_test.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper'
+
+class CommandLineInterface < MiniTest::Test
+
+  def test_it_can_find_by_attribute_and_value
+    a = AttendeeRepo.new(records)
+    result = a.find(first_name: 'Aya').first
+    assert_equal 'Aya', result.first_name
+    assert_equal 'Fuller', result.last_name
+  end
+
+  def test_it_can_call_on_help_command
+  end
+
+  def test_it_can_count_records_in_queue
+  end
+
+  def test_it_can_print_queue
+  end
+
+  def test_it_can_save_to_csv
+  end
+
+  def test_it_will_return_
+
+
+end
