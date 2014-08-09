7512e9f79b6fe75e905cb27f0af3da52b6f4274a
Jessica Goulding <jgoulding2@gmail.com>
2014-08-08 23:05:19 -0600

    testing for attendees and repo

--- test/attendee_repo_test.rb
+++ test/attendee_repo_test.rb
@@ -2,9 +2,28 @@ require_relative 'test_helper'
 
 class AttendeeRepoTest <MiniTest::Test
 
-  def test_it_can_build_records
+  def test_it_can_create_a_csv
+    repo = AttendeeRepo.new(nil)
+    repo.load('./test/event_attendess_test.csv')
+    assert repo.csv.is_a? CSV
+  end
 
-    assert true, build_records
+  def test_it_doesnt_load_a_nonexisting_file
+    repo = AttendeeRepo.new(nil)
+    repo.load('./test/event_attendess_test.csv')
+    assert_equal 0, repo.csv.count
+  end
+
+  def test_it_builds_entries
+    repo = AttendeeRepo.new(value)
+    repo.load('./test/fevent_attendess_test.csv')
+    repo.build_entries
+    assert_equal 4, repo.entries.count
+  end
+
+  def test_it_sets_the_attributes
+    repo = AttendeeRepo.new(value)
+    assert_equal [:first_name], repo.attributes
   end
 
 
--- test/attendee_test.rb
+++ test/attendee_test.rb
@@ -2,12 +2,18 @@ require_relative 'test_helper'
 
 class Attendee <MiniTest::Test
 
-  def test_it_can_build_records
-    assert true, build
-  end
-
-  def test_it_can_load_csv
-
+  def test_it_can_clean_attributes
+    attendee = Attendee.build(row)
+    assert cleaner[:_], attendee.id
+    assert cleaner[:regdate], attendee.regdate
+    assert cleaner[:first_name], attendee.first_name
+    assert cleaner[:last_name], attendee.last_name
+    assert cleaner[:homephone], attendee.homephone
+    assert cleaner[:email_address], attendee.email_address
+    assert cleaner[:street], attendee.street
+    assert cleaner[:city], attendee.city
+    assert cleaner[:state], attendee.state
+    assert cleaner[:zipcode], attendee.zipcode
   end
 
 end
