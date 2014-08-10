56e23f2a5858a3f2e32699ce6db48de516bf7142
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-08-09 20:01:53 -0600

    added tests and refactored cleaning methods a bit

--- lib/cleaner.rb
+++ lib/cleaner.rb
@@ -1,20 +1,35 @@
 class Cleaner
 
+	def first_name_cleaner(first_name)
+		first_name.downcase
+	end
+
   def clean_zipcode(zipcode)
- 	  zipcode.to_s.rjust(5,"0")	[0..4]
+ 	  zipcode.to_s.rjust(5,"0")[0..4]
   end
 
   def clean_phone_number(phone_number)
-	  if phone_number =~ /[^0-9]/
-	   phone_number = phone_number.gsub(/[^0-9]/,"")
-	  end
-	  
-	  if phone_number.length == 10
-	    phone_number
-	  elsif phone_number.length < 10 || phone_number.length > 11  
-	    "Bad Number"	    	  
-	  elsif phone_number.length == 11 && phone_number[0] == "1"
-	    phone_number[1..-1]
+    
+    only_digits = remove_non_digit_characters(phone_number)
+	  if only_digits.length == 10
+	    only_digits
+	  elsif only_digits.length < 10 || only_digits.length > 11
+	    "Bad Number"
+	  elsif only_digits.length == 11 && only_digits[0] == "1"
+	    only_digits[1..-1]
     end
   end
-end
+
+  def remove_non_digit_characters(phone_number)
+    if phone_number =~ /[^0-9]/
+	    phone_number = phone_number.gsub(/[^0-9]/,"")
+	  end
+	  phone_number
+  end
+end  
+
+
+
+
+
+
