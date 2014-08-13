0f92a2896aa21ed76eb8ca5864877c8f549e87df
Jessica Goulding <jgoulding2@gmail.com>
2014-08-13 10:07:30 -0600

    print by works

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,34 +2,25 @@ require 'terminal-table'
 require_relative 'attendee'
 
 class Queue
-  include MessagePrinter 
+  include MessagePrinter
   attr_accessor :attendees
 
   def initialize
     @attendees = []
   end
 
-  def save_by(filename)
-    #save
-  end
-
   def clear
     attendees.clear
   end
 
-  # def print
-  #
-  #   # rows = attendees.collect {|attendee| puts attendee.inspect; attendee.to_row }
-  #   attendees
-  #   # table = Terminal::Table.new :rows => rows
-  # end
+  def count
+    attendees.count
+  end
 
   def print
     if @attendees.empty?
         puts MessagePrinter.print_error_message
     else
-      #@attendees.each { |attendee| puts attendee.id  }
-
       rows = []
       @attendees.each do |a|
         rows << [
@@ -50,14 +41,33 @@ class Queue
     end
   end
 
-  def queue_print_by_attribute(attribute)
-    queue.sort_by! do |record|
-      record.send(attribute)
+  def print_by(attribute)
+    attendees.sort_by! do |attendee|
+      attendee.send(attribute)
     end
     queue_print
   end
 
-  def count
-    attendees.count
+  def save(filename)
+    CSV.open(create_filename(filename), 'w') do |csv|
+    csv << ['ID', 'RegDate', 'First Name', 'Last Name', 'Email Address', 'Home Phone', 'Street', 'City', 'State', 'Zip Code']
+    @attendees.each do |a|
+      csv << [
+        "#{a.id}",
+        "#{a.regdate}",
+        "#{a.first_name}",
+        "#{a.last_name}",
+        "#{a.email_address}",
+        "#{a.homephone}",
+        "#{a.street}",
+        "#{a.city}",
+        "#{a.state}",
+        "#{a.zipcode}"
+        ]
+      end
+    end
   end
+
+
+
 end
