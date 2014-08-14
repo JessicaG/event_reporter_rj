119b6a269531464ca72503f8fb75a323c586698f
Jessica Goulding <jgoulding2@gmail.com>
2014-08-14 09:14:06 -0600

    argh merge conflicts, please work

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -2,32 +2,39 @@ require_relative 'test_helper'
 require_relative '../lib/queue'
 
 class QueueTest < Minitest::Test
-	attr_reader :queue
-	
+  attr_reader :queue, :person
+
+  def setup
+        @queue = Queue.new
+
+   @person = {
+     first_name:"Allison",
+     last_name: "Nguyen",
+     email_address:"arannon@jumpstartlab.com",
+     homephone: "615.438-5000",
+     street: "3155 19th St NW",
+     city: "Washington",
+     state: "DC",
+     zipcode: "20010"}
+   end
+
 	def test_it_exists
-    assert Queue
+   assert Queue
   end
 
   def test_user_command_initializes_an_empty_array
-    @queue = Queue.new
-    assert_equal [], queue.attendees
-  end  
+   assert_equal [], queue.attendees
+  end
 
 
-	# def test_it_loads_the_data
-	# 	# We want to call/ count load up data in order to count it.
-	# end
+  def test_it_adds_to_attendees
+  	queue.add_to_attendees(person)
+  	assert_equal "Allison", queue.attendees.first[1]
+  end
 
-	def test_it_can_clear_a_queue
-    
-	end
+  def test_it_can_clear_a_queue
+   queue.attendees <<  person
+	 assert_equal [], queue.clear
+  end
 
-	# def test_it_can_print
-		
-	# 	# assert TerminalTable.new()
-	# end
-
-	# def test_it_can_save_to_csv
-	# 	# calling queue save saves the queue to a file
-	# end
 end
